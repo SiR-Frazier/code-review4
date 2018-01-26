4fbe97a8219b5686baa8d63aae08e7e83cff15cf
Bryce Frazier <bx.frazier@gmail.com>
2018-01-26 14:54:44 -0800

    update html, css, js. js backend logic for pizza size works.

--- js/scripts.js
+++ js/scripts.js
@@ -22,44 +22,69 @@ function userInput() {
 //size cost: 12: xL, 10: l, 8: md, 6: sm
 //topping cost: 5: meat Tornado, sausage/pepperoni: 3, cheese: 2
 
-PizzaOrder.prototype.price = function{
+PizzaOrder.prototype.price = function(){
   var cost = 0;
-  if (this.size === "Xtra Large" && this.topping === "Meat Tornado") {
-    cost = 17;
-  } else if (this.size === "Large" && this.topping === "Sausage" || this.topping === "Pepperoni") {
-    cost = 15;
-  } else {
-    cost = 14;
-  }
-  if (this.size === "Large" && this.topping === "Meat Tornado") {
-    cost = 15;
-  } else if (this.size === "Large" && this.topping === "Sausage" || this.topping === "Pepperoni") {
-    cost = 13;
-  } else {
+   //xtra large
+  if (this.size === "Xtra Large: Bring Da Ruckus") {
     cost = 12;
-  }
-  if (this.size === "Medium" && this.topping === "Meat Tornado") {
-    cost = 13;
-  } else if (this.size === "Medium" && this.topping === "Sausage" || this.topping === "Pepperoni") {
-    cost = 11;
-  } else {
-      cost = 10;
-  }
-  if (this.size === "Small" && this.topping === "Meat Tornado") {
-    cost = 11;
-  } else if (this.size === "Small" && this.topping === "Sausage" || this.topping === "Pepperoni") {
-    cost = 9;
-  } else {
+  } else if (this.size === "Large") {
+    cost = 10;
+  } else if (this.size === "Medium") {
     cost = 8;
+  } else if (this.size === "Select Size") {
+    alert("What are you tryin' to pull?"); 
+  } else {
+    cost = 6;
   }
+  if (this.topping === "Meat Tornado: everything but the kitchen sink!") {
+    cost + 5;
+  } else if (this.topping === "Sausage" || this.topping === "Pepperoni") {
+    cost + 3;
+  } else {
+    cost + 2;
+    }
     return cost;
-}
+  }
+  // if (this.size === "Xtra Large: Bring Da Ruckus" && this.topping === "Meat Tornado: everything but the kitchen sink!"){
+  //   cost = 17;
+  // } else if (this.size === "Xtra Large: Bring Da Ruckus" && this.topping === "Sausage" || this.topping === "Pepperoni") {
+  //   cost = 15;
+  // } else {
+  //   cost = 14;
+  // }
+  // //large
+  // if (this.size === "Large" && this.topping === "Meat Tornado: everything but the kitchen sink!") {
+  //   cost = 15;
+  // } else if (this.size === "Large" && this.topping === "Sausage" || this.topping === "Pepperoni") {
+  //   cost = 13;
+  // } else {
+  //   cost = 12;
+  // }
+  // //medium
+  // if (this.size === "Medium" && this.topping === "Meat Tornado: everything but the kitchen sink!") {
+  //   cost = 13;
+  // } else if (this.size === "Medium" && this.topping === "Sausage" || this.topping === "Pepperoni") {
+  //   cost = 11;
+  // } else {
+  //     cost = 10;
+  // }
+  // //small
+  // if (this.size === "Small: Are you kiddin' me?" && this.topping === "Meat Tornado: everything but the kitchen sink!") {
+  //   cost = 11;
+  // } else if (this.size === "Small" && this.topping === "Sausage" || this.topping === "Pepperoni") {
+  //   cost = 9;
+  // } else {
+  //   cost = 8;
+  // }
+  //   return cost;
+  // }
 
 //front-end logic:
 $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
-    var order = userInput();  
-  }
-)
+    var userOrder = userInput();
+    console.log(userOrder);
+    $("#order").text("Bring Da Ruckus! Your total is: $" + userOrder.price());
+  });
 });
