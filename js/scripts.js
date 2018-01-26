eb099af99b95b3ea6a667875128c5022db9c5ddf
Bryce Frazier <bx.frazier@gmail.com>
2018-01-26 09:08:49 -0800

    outline of back-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 //specs:
 //the user will select a size for the pizza using one of the buttons
+// write constructor for pizza order;
+// write function that allows user input to be registered on the form;
+// write protoype method for the price of each pizza size and for the topping(s);
+
 //back-end logic:
+function PizzaOrder(size, crust, topping) {
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+}
+
+function userInput() {
+  var inputtedSize = $("#size").val();
+  var inputtedCrust = $("#crust").val();
+  var inputtedTopping = $("#topping").val();
+  var newOrder = new PizzaOrder(inputtedSize, inputtedCrust, inputtedTopping);
+
+  return newOrder
+}
+
 
 //front-end logic:
+$(document).ready(function() {
+
+});
