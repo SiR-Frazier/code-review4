d9d9b6dad1b0488fe2b3cd4c7d0028a5e3f11312
Bryce Frazier <bx.frazier@gmail.com>
2018-01-26 09:29:38 -0800

    add prototype for price in backend

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,39 @@
 // write protoype method for the price of each pizza size and for the topping(s);
 
 //back-end logic:
-function PizzaOrder(size, crust, topping) {
+function PizzaOrder(size, topping) {
   this.size = size;
-  this.crust = crust;
   this.topping = topping;
 }
 
 function userInput() {
   var inputtedSize = $("#size").val();
-  var inputtedCrust = $("#crust").val();
   var inputtedTopping = $("#topping").val();
-  var newOrder = new PizzaOrder(inputtedSize, inputtedCrust, inputtedTopping);
+  var newOrder = new PizzaOrder(inputtedSize, inputtedTopping);
 
   return newOrder
 }
 
+PizzaOrder.prototype.price = function{
+  var cost = 0;
+  if (this.size === "Xtra Large") {
+    cost = 12;
+  } else if (this.size === "Large") {
+    cost = 10;
+  } else if (this.size === "Medium") {
+    cost = 8;
+  } else {
+    cost = 6;
+  }
+
+  if (this.topping === "Meat Tornado") {
+    cost = 5;
+  } else if (this.topping === "Pepperoni" || this.topping === "Sausage") {
+    cost = 3;
+  } else {
+    cost = 2;
+  }
+}
 
 //front-end logic:
 $(document).ready(function() {
